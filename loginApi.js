5b0940e56b9e143bb3c577b9825d558dd7cae677
SMT <mohamedtawfeeqprogrammer25@gmail.com>
2023-01-16 14:13:58 +0530

    login api

---
+++ loginApi.js
@@ -0,0 +1,44 @@
+/*
+	Login page api
+	express js
+	mysql database
+*/
+const express=require('express');
+const server=express();
+const body=require('body-parser');
+const mysql=require('mysql');
+server.use(body.json());
+/*connecting to mysql server*/
+var sql=mysql.createConnection({host:/*Host Name*/,user:/*username*/,password:/*password*/,database:/*database name*/});
+console.log('db connected');
+sql.connect();
+/*login api*/
+/*http post method*/
+server.post('/Login',(req,res)=>{
+	/*payload data email and password {email:'email',password:'password'}*/
+	sql.query("select count(rest_id) as data,convert(aes_decrypt(password,'quickreadscan'),char) as pass  from userlog where email=?",[req.body.email],(err1,ret1)=>{
+		/*the password in the db is encrypted with aes_encrypt()*/
+		if(err1==null)
+		{
+				/*checking if the account exists*/
+				if(ret1[0].data==1)
+				{
+					/*validating the password*/
+					if(req.body.password==ret1[0].pass){
+						console.log('access granted');
+					}
+					else
+					{
+					console.log('invalid password');
+					}
+				}
+				else
+				{
+				console.log('account not found');
+			}
+		}
+		else{
+			console.log(err1);
+		}
+	});
+});
